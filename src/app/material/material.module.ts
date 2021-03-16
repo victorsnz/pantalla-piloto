b9c1f6215cbd824cfae1b459148e8d91a789877a
Victor Sanchez <victor_sanchez@outlook.com>
2021-03-16 16:43:36 -0300

    material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,38 @@
 import { NgModule } from '@angular/core';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSortModule } from '@angular/material/sort';
+
 
 const Material =[
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatFormFieldModule,
+  MatPaginatorModule,
+  MatCheckboxModule,
+  MatGridListModule,
   MatToolbarModule,
-  MatIconModule,
   MatButtonModule,
-  MatGridListModule
+  MatTableModule,
+  MatInputModule,
+  MatIconModule,
+  MatMenuModule,
+  MatSortModule,
 ]
 @NgModule({
   imports: [Material],
   exports: [Material]
 })
+
 export class MaterialModule { }
